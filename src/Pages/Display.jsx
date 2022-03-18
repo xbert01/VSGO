356558b1f39428c1991899d47da88c48fa01726f
Fabian <frzadkowski@wp.pl>
2022-03-18 09:46:36 +0100

    restructure

--- src/Pages/Display.jsx
+++ src/Pages/Display.jsx
@@ -6,97 +6,56 @@ import Shuffle from "../Elements/Shuffle";
 import CarData from "../Elements/CarData";
 import { EmptyFile } from "../Elements/EmptyFile";
 
+
+
+
 function Display() {
-  //Left Container Default Cars
-  let [startLeft, setSkipLast] = React.useState(0); //Sets Min object index to be displayed in Left Container
-  let [endLeft, setShowNext] = React.useState(1); //Sets Max object index to be displayed in Left Container
-  //Right Container Default Cars
-  let [startRight, setSkipLastRight] = React.useState(1); //Sets Min object index to be displayed in Right Container
-  let [endRight, setShowNextRight] = React.useState(2); //Sets Max object index to be displayed in Right Container
 
-  // const BackToStart = () => {
-  //   return (
-  //     (startLeft = -1),
-  //     (endLeft = 0),
-  //     (startRight = 0),
-  //     (endRight = 1)
-  //   );
+  // const setShuffle = (r) => {
+  //   for (let i = r.length - 1; i > 0; i--) {
+  //     const j = Math.floor(Math.random() * (i + 1));
+  //     const temp = r[i];
+  //     r[i] = r[j];
+  //     r[j] = temp;
+  //   }
+  //   return r;
   // };
+  // const Reshuffled = () => {
+  //   return setShuffle(CarData);
+  // };
+  // Reshuffled();
+  
+  
+  
+  let a = CarData[0];
+  let b = CarData[2];
+  let c = CarData.slice(0, 1);
+  let d = CarData.slice(1, 2);
+  
+    console.log(a.year)
+    console.log(b)
+    console.log(c[0].year)
+    console.log(d)
+  
 
-  const showNextData = () => {
-    //Function displays Next Object from an Array-Cars(Next Car)
-    //Left Container Next Car
-    setSkipLast(startLeft + 1);
-    setShowNext(endLeft + 1);
+   function removeFirst() {
+     CarData.shift();
+     console.log(CarData)
+  } 
 
-    //Right Container Next Car
-    setSkipLastRight(startRight + 1);
-    setShowNextRight(endRight + 1);
-  };
-
-  let rightValue = 2;
-  let leftValue = 2;
-  let counter = 0;
-
-  function goToGameOverPage() {
-    console.log("they are not the same");
-  }
-
-  function Equal() {
-    if (leftValue == rightValue) {
-      ++counter;
-      console.log(counter);
-    } else goToGameOverPage();
-    return counter;
-  }
-
-  // Function shuffles order of the array
-  const setShuffle = (r) => {
-    for (let i = r.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      const temp = r[i];
-      r[i] = r[j];
-      r[j] = temp;
-    }
-    return r;
-  };
-  const Reshuffled = () => {
-    return setShuffle(CarData);
-  };
-
-  // setShuffle(CarData);
-  Reshuffled();
-
-  // Refreshing function restarts the game
-  function refreshPage() {
-    window.location.reload();
-  }
-  const leftData = () => {
-    const a = CarData.slice(startLeft, endLeft);
-    return a;
-  };
-
-  const rightData = () => {
-    const b = CarData.slice(startRight, endRight);
-    return b;
-  };
-
-  console.log(leftData().price);
-
-  console.log(setShuffle(CarData));
   return (
     <>
       <div className='carScreens'>
         <Buttons />
         <div className='carRandom'>
-          {EvenFile(leftData())}
+          {EvenFile(c)}
         </div>
         <VersusBar />
         <div className='carRandom'>
-          {EmptyFile(rightData())}
+          {EmptyFile(d)}
         </div>
       </div>
-
+ 
       <div
         style={{
           display: "flex",
@@ -105,23 +64,15 @@ function Display() {
           backgroundColor: "var(--blueDark)",
         }}
       >
-        {/* <button
-          className='button button-lower'
-          onClick={() =>
-            BackToStart() & console.log(BackToStart())
-          }
-        >
-          Restart Game
-        </button> */}
-        <button
+         {/* <button
           className='button button-higher'
           onClick={() => showNextData()}
         >
           Next
-        </button>
+        </button> */}
         <button
           className='button button-even'
-          onClick={() => Equal()}
+          onClick={() => removeFirst()}
         >
           Equal
         </button>
