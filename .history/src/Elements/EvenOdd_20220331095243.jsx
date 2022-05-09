e5ed77ae0b1587cc5df1c514fae96397c5872079
Fabian <frzadkowski@wp.pl>
2022-05-09 17:22:45 +0200

    Update EvenOdd_20220331095243.jsx

--- .history/src/Elements/EvenOdd_20220331095243.jsx
+++ .history/src/Elements/EvenOdd_20220331095243.jsx
@@ -1,9 +1,19 @@
 const showData = (data) => (
   <>
+<<<<<<< Updated upstream:.history/src/Elements/EvenOdd_20220331095243.jsx
     {data.map((car) => (
       <div className='pack-term' style={{ backgroundImage: 
         `url(${car.img})`
       }}>
+=======
+    {data.map((car, index) => (
+      <div
+        className='itemContainer'
+        id='item1'
+        key={index + 1}
+        style={{ backgroundImage: `url(${car.img})` }}
+      >
+>>>>>>> Stashed changes:src/Elements/EvenOdd.jsx
         <div className='overlay'>
           <div className='itemDataContainer' key={car.id}>
             <img src={car.logo} alt='logo' />
@@ -12,15 +22,15 @@ const showData = (data) => (
             </div>
             <h4>{car.year}</h4>
             <h2>Max Speed is</h2>
-            <h2 style={{ color:"var(--magenta)" }}>{car.speed}km/h</h2>
+            <h2 style={{ color: "var(--magenta)" }}>
+              {car.speed}km/h
+            </h2>
           </div>
         </div>
       </div>
     ))}
   </>
 );
-
+//hello world
 export const EvenFile = (data) => <>{showData(data)}</>;
-
-
 
