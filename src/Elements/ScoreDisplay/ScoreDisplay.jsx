ea6cd25f5b350de244fc11ffb4833536afdfa8f1
Hubert <90800796+xbert01@users.noreply.github.com>
2022-07-10 21:23:20 +0200

    few media query changes

--- src/Elements/ScoreDisplay/ScoreDisplay.jsx
+++ src/Elements/ScoreDisplay/ScoreDisplay.jsx
@@ -1,8 +1,8 @@
 const ScoreDisplay = (props) => {
   return (
     <div style={{ textAlign: "center", margin: "2em 3em" }}>
-      <h3>{props.text}</h3>
-      <h3 style={{ color: "var(--magenta)" }}>
+      <h3 className="scoreHeading">{props.text}</h3>
+      <h3 className="scoreHeading"style={{ color: "var(--magenta)" }}>
         {props.score}
       </h3>
     </div>
